48f898e37f48abab26b956d02d6b6386212f23de
hungnd_dev0612 <hungnd.dev0612@gmail.com>
2026-03-16 22:26:19 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+import {
+  createIcons,
+  pause,
+  play,
+  shuffle,
+  skipBack,
+  skipForward,
+} from "lucide";
+
+createIcons({
+  icons: {
+    skipBack,
+    pause,
+    shuffle,
+    play,
+    skipForward,
+  },
+});
