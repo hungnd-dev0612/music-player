0597ac71f639b537442ed435fa9fcd1bb2d12d03
hungnd_dev0612 <hungnd.dev0612@gmail.com>
2026-03-20 00:39:44 +0700

    fix bundle directory

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
-    publicPath: process.env.NODE_ENV === "production" ? "/music-player/" : "/",
+    publicPath: process.env.NODE_ENV === "production" ? "./" : "/",
   },
 
   plugins: [
